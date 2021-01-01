51dbd3af611249322b25b4b48cd7c62a9e9b8291
AndrinMiftari <74106749+AndrinMiftari@users.noreply.github.com>
2021-01-01 02:34:19 +0100

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+function ThemeZeze() {
+	var link = document.createElement('link');
+	link.rel = "stylesheet";
+	link.type = "text/css";
+	link.href = "css/zeze.css";
+	document.querySelector("head").appendChild(link);
+}
+
+function ThemeNormale() {
+	var link = document.createElement('link');
+	link.rel = "stylesheet";
+	link.type = "text/css";
+	link.href = "css/normale.css";
+	document.querySelector("head").appendChild(link);
+}
+
+function NdryshoBackground() {
+	var kohaTani = new Date().getHours();
+	if(20 <= kohaTani && kohaTani < 24) {
+		document.write("<style>html {background-color:#121212;}\n\ ");
+		document.write(".columnist h1{background-color: #121212;}</style> ");
+	}
+	if(0 <= kohaTani && kohaTani < 12) {
+		document.write("<style>html {background-color:#121212;}\n\ ");
+		document.write(".columnist h1{background-color: #121212;}</style> ");
+	}
+}
+NdryshoBackground();
